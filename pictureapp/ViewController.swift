63e8c10c42fc90b208851dc967588441a3b985bb
lpmetor <lpmetor@gmail.com>
2019-10-19 14:56:07 -0400

    Initial Commit

---
+++ pictureapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pictureapp
+//
+//  Created by jerry on 10/19/19.
+//  Copyright © 2019 lpmetor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
